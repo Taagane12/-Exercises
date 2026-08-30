fd66acd1cef38f392ed53233239735899bbf5f5a
Taagane12 <mabdi7897@gmail.com>
2026-08-30 17:42:24 +0300

    first commit

---
+++ Exercise 1/main.js
@@ -0,0 +1,16 @@
+let x = 7;
+let y = 3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+// pre increment
+console.log(++x);
+// post increment
+console.log(x++);
+console.log(x);
+// pre decrement
+console.log(--x);
+// post decrement 
+console.log(x--);
