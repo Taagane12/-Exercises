883bdbe9c0c81e925507f2b9a250dec92a40e05c
Taagane12 <mabdi7897@gmail.com>
2026-09-02 15:04:16 +0300

    Exercise 3

--- Exercise 3/main.js
+++ Exercise 3/main.js
@@ -1,6 +1,6 @@
 let isSunny = true;
 let isWeekend = true;
 if (isSunny && isWeekend)
-    console.log("you can go to beach")
+    console.log("you can go to beach.")
 else
     console.log("you can not");
