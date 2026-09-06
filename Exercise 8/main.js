d1f1e5b5cd3bb233b6234884faed69c10b1fa15e
Taagane12 <mabdi7897@gmail.com>
2026-09-06 19:38:04 +0300

    Exercise 8

---
+++ Exercise 8/main.js
@@ -0,0 +1,8 @@
+const car = {
+    Make: "toyoto",
+    Model: "lancruiser",
+    year: 2006,
+};
+console.log("make: " + car.Make);
+console.log("model: " + car.Model);
+console.log("year: " + car.year);
